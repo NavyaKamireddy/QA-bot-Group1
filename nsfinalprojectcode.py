c4488fa084d0c83900aaaaae8609ae880293b070
NavyaKamireddy <146391323+NavyaKamireddy@users.noreply.github.com>
2023-12-01 13:24:22 -0600

    Update nsfinalprojectcode.py
    Signed-off-by: NavyaKamireddy <146391323+NavyaKamireddy@users.noreply.github.com>

--- nsfinalprojectcode.py
+++ nsfinalprojectcode.py
@@ -24,14 +24,18 @@ logger.setLevel(logging.DEBUG)
 
 # Graphsignal: configure
 import graphsignal
-graphsignal.configure(api_key='GRAPHS_SIGNAL_API_KEY', deployment='DEPLOYMENT_NAME')
+graphsignal.configure(api_key='GRAPHSIGNAL_API_KEY', deployment='YOUR_DEPLOYEMENT_NAME')
+
+#creating the environment
+import openai  # Import openai library
+os.environ["OPENAI_API_KEY"] = "OPENAI_API_KEY"
 
 #creating the environment
 import openai  # Import openai library
 os.environ["OPENAI_API_KEY"] = "OPENAI_API_KEY"
 
 # Function to extract text from PDF using PyMuPDF
-def extract_text_from_pdf(pdf_path_1):
+def extract_text_from_pdf_1(pdf_path_1):
     with fitz.open(pdf_path_1) as doc_lecture:
         for page_number in range(doc_lecture.page_count):
             page = doc_lecture[page_number]
@@ -39,21 +43,39 @@ def extract_text_from_pdf(pdf_path_1):
             yield page_number + 1, page_text
           
             
-def etracting_text_from_pdf(pdf_path_2):
-    with fitz.open(pdf_paths_2) as docs_textbook:
-        for page_numbers in range (docs_textbook.page_counts):
+def extract_text_from_pdf_2(pdf_path_2):
+    with fitz.open(pdf_path_2) as docs_textbook:
+        for page_numbers in range(docs_textbook.page_count):
             pages = docs_textbook[page_numbers]
             page_texts = pages.get_text()
             yield page_numbers + 1, page_texts
-# Function to extract metadata from document text
-def extract_metadata(text, pattern):
-    with graphsignal.start_trace('extract_metadata') as span:
-        match = re.search(pattern, text)
-        page_number = match.group(1) if match else "Unknown Page"
-        span.set_data('page_number', page_number)
-        span.set_data('pattern', pattern)
-        return f"{pattern} {page_number}" if match else f"Unknown {pattern}"
 
+#extracting metadata from the path_pdf_1 which is NS_All_slides
+def extract_metadata_1(text, patterns_1):
+    with graphsignal.start_trace('extract_metadata_1') as span:
+        metadata = {}
+        for key, pattern in patterns_1.items():
+            match = re.search(pattern, text)
+            metadata[key] = match.group(1) if match else "Unknown"
+            span.set_data(key, metadata[key])
+            span.set_data(f'{key}_pattern', pattern)
+
+        return metadata
+#extracting metadata from the path_pdf_1 which is NS_Textbook
+def extract_metadata_2(text, patterns_2):
+    with graphsignal.start_trace('extract_metadata_2') as span:
+        metadata = {}
+        for key, pattern in patterns_2.items():
+            match = re.search(pattern, text)
+            metadata[key] = match.group(1) if match else "Unknown"
+            span.set_data(key, metadata[key])
+            span.set_data(f'{key}_pattern', pattern)
+
+        return metadata
+
+
+
+#given input generating question
 def generate_questions(text):
     sentences = text.split('.')
     questions = [f"   {sentence}?" for sentence in sentences if sentence.strip()]
@@ -70,13 +92,14 @@ def generate_chatgpt_answer(question):
     )
     chatgpt_answer = response.choices[0].text.strip()
     return chatgpt_answer
+
 pdf_path_1 = 'LOCAL_PATH.pdf'
 pdf_path_2 = "LOCAL_PATH.pdf"
 try:
     start_time = time.time()
 
     # Extract text from the first PDF using PyMuPDF
-    extracted_text_1 = extract_text_from_pdf(pdf_path_1)
+    extracted_text_1 = extract_text_from_pdf_1(pdf_path_1)
     # Combine all text from different pages into a single string
     combined_text_1 = ' '.join(page_text for _, page_text in extracted_text_1)
 
@@ -109,8 +132,14 @@ try:
     answer_from_document = chain_1.run(input_documents=doc_lecture, question=user_question)
     
     if answer_from_document.strip():
-        with graphsignal.start_trace('extract_metadata') as span:
-            metadata_1 = extract_metadata(combined_text_1)
+        # Example: Extracting Lecture, Page Number, 
+        patterns_1 = {
+            'lecture': r'Lecture (\d+)',
+            'page_number': r'Page (\d+)',
+           
+        }
+        with graphsignal.start_trace('extract_metadata_1') as span:
+            metadata_1 = extract_metadata_1(combined_text_1, patterns_1)
             span.set_data('metadata_1', metadata_1)
 
         print(f"Metadata from NS_All_Slides : {metadata_1}")
@@ -121,7 +150,7 @@ try:
         print("-" * 50)
 
     # Extract text from the second PDF using PyMuPDF
-    extracted_text_2 = extract_text_from_pdf(pdf_path_2)
+    extracted_text_2 = extract_text_from_pdf_2(pdf_path_2)
     # Combine all text from different pages into a single string
     combined_text_2 = ' '.join(page_texts for _, page_texts in extracted_text_2)
     # Split text into smaller chunks
@@ -151,8 +180,14 @@ try:
 
 
     if answer_from_textbook.strip():
-        with graphsignal.start_trace('extract_metadata') as span:
-            metadata_2 = extract_metadata(combined_text_2)
+        # Example: Page Number, and Chapter Number
+        patterns_2 = {
+            
+            'page_number': r'Page (\d+)',
+            'chapter_number': r'Chapter (\d+)'
+        }
+        with graphsignal.start_trace('extract_metadata_2') as span:
+            metadata_2 = extract_metadata_2(combined_text_2, patterns_2)
             span.set_data('metadata_2', metadata_2)
 
         print(f"Metadata from : {metadata_2}")
@@ -175,4 +210,3 @@ try:
 
 except Exception as e:
     print(f"Error processing {pdf_path_1} or {pdf_path_2}: {str(e)}")
-
